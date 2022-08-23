99731101f2e41c3a1a0b5695cb4e0db4a4bb21e7
renatodevops <renatodevops@gmail.com>
2022-08-23 17:08:56 -0300

    remove .vagrant/

--- kubernetes/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"e7504459776e32ac11b97f71791ef89d9abaee0e665c0b911721bde98fb0aa68","vagrant_version":"2.3.0"}
